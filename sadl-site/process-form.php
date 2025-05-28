3f0cabe27f890e6d7ef1f51572516e20094c2fad
Greg Swift <gregswift@gmail.com>
2025-05-28 14:11:07 -0500

    fix(process-form): fix processing of the recaptcha and ensuring sending mail works

--- sadl-site/process-form.php
+++ sadl-site/process-form.php
@@ -1,4 +1,10 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 
 require 'vendor/autoload.php';
 require_once '../config.inc.php';
@@ -7,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
         $token = $_POST['g-recaptcha-response'];
-        error_log('Token: '.$token);
+        // error_log('Token: '.$token);
         $url = 'https://recaptchaenterprise.googleapis.com/v1/projects/websites-459715/assessments?key='.$recaptchaAPIKey;
         $data = [
           "event" => [
@@ -15,26 +21,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             "siteKey" => $recaptchaSiteKey,
           ]
         ];
-
+        $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'Direct Visit';
+        // error_log('data:'.$data);
         $options = [
             'http' => [
                 'method' => 'POST',
-                'header' => 'Content-Type: application/json\r\n',
-                'content' => http_build_query($data),
+                'header' => [
+                  'Content-Type: application/json',
+                  "Referer: $referer\r\n",
+                ],
+                'content' => json_encode($data),
+                'ignore_errors' => true,
             ]
         ];
+        // var_dump($options);
         $context = stream_context_create($options);
         $verify = file_get_contents($url, false, $context);
         $captcha_success = json_decode($verify);
-
-        if ($captcha_success->success) {
+        // error_log('results: '.$verify);
+        // var_dump($captcha_success);
+        if ($captcha_success->riskAnalysis->score >= 0.5) {
             $name = $_POST['name'];
             $email = $_POST['email'];
             $message = $_POST['message'];
 
             $mail = new PHPMailer;
 
-            // $mail->SMTPDebug = 3;                               // Enable verbose debug output
+            $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = $smtpHost;  // Specify main and backup SMTP servers
@@ -44,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->SMTPSecure = $smtpSecure;                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = $smtpPort;                                    // TCP port to connect to
 
-            $mail->setFrom($smtpFromAddress, $smtpFromName);
+            $mail->setFrom($smtpUsername, $smtpFromName);
             $mail->addAddress($smtpTarget);     // Add a recipient
             $mail->addReplyTo($email, $name);
             $mail->addBCC($smtpTarget);
