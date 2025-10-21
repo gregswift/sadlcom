b2cad511c98e65de5ca5e19411b421f08bb1f45e
Greg Swift <gregswift@gmail.com>
2025-10-21 16:32:49 -0500

    fix: chrome wasnt treating this as closing, so it was wrapping the next script

--- sadl-site/contact.php
+++ sadl-site/contact.php
@@ -123,7 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <style>
       @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap");
     </style>
-    <script src="https://www.google.com/recaptcha/api.js?render=<?php echo urlencode($recaptchaSiteKey); ?>" async="" defer="" />
+    <script src="https://www.google.com/recaptcha/api.js?render=<?php echo urlencode($recaptchaSiteKey); ?>" async="" defer=""></script>
     <script>
 function handleSubmit(e) {
   e.preventDefault();
