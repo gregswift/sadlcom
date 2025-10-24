c2da32971cc9efb88929aba7568a060d99fd0845
Greg Swift <gregswift@gmail.com>
2025-10-24 12:11:52 -0500

    feat: Significant refactor of php config importing and adding schema.org specific to each page

--- sadl-site/head.inc.php
+++ sadl-site/head.inc.php
@@ -1,7 +1,3 @@
-<?php
-  $metaTitle       = $pageTitle . ' | San Antonio Disability Lawyer | Michael Archer';
-  $metaDescription = 'Experienced and trusted legal help with your disability claim. Get what you deserve. Call Michael Archer now at '. $phoneNumber;
-?>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php echo $metaTitle; ?></title>
@@ -25,73 +21,7 @@
 </script>
 <!-- schema.org representation -->
 <script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": ["LegalService", "Attorney"],
-  "name": "Michael Archer, San Antonio Disability Lawyer",
-  "url": "https://sanantoniodisabilitylawyer.com/",
-  "description": "San Antonio Attorney Michael Archer helps clients secure Social Security and disability benefits across Texas. Call for trusted representation.",
-  "telephone": "<?php echo $phoneNumber; ?>",
-  "priceRange": "$",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "300 E Ramsey Rd, Suite 305",
-    "addressLocality": "San Antonio",
-    "addressRegion": "TX",
-    "postalCode": "78216",
-    "addressCountry": "US"
-  },
-  "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": 29.5282,
-    "longitude": -98.4938
-  },
-  "areaServed": "San Antonio, Texas",
-  "contactPoint": [
-    {
-      "@type": "ContactPoint",
-      "contactType": "Mailing",
-      "email": "contact@sanantoniodisabilitylawyer.com",
-      "availableLanguage": "English",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "1150 N Loop 1604 W, #108-420",
-        "addressLocality": "San Antonio",
-        "addressRegion": "TX",
-        "postalCode": "78248",
-        "addressCountry": "US"
-      }
-    }
-  ],
-  "openingHoursSpecification": [
-    {
-      "@type": "OpeningHoursSpecification",
-      "dayOfWeek": [
-        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
-      ],
-      "opens": "09:00",
-      "closes": "17:00"
-    }
-  ],
-  "founder": {
-    "@type": "Person",
-    "name": "Michael Archer"
-  },
-  "makesOffer": [
-    {
-      "@type": "Offer",
-      "itemOffered": {
-        "@type": "Service",
-        "name": "Social Security Disability Representation",
-        "serviceType": "Disability Law"
-      }
-    }
-  ],
-  "sameAs": [
-    "https://linkedin.com/in/michael-archer-b5605341"
-  ],
-  "hasMap": "https://maps.app.goo.gl/judsNN9U8g1h3CK6A"
-}
+<?php echo $schema; ?>
 </script>
 <!-- Styling -->
 <link rel="stylesheet" href="/public/css/normalize.css">
